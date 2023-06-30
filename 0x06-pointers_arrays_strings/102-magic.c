244a7edc42819fcb681dea39115eca0e85725d3a
Kingston <paaquesi143@gmail.com>
2023-06-30 04:03:26 -0700

    Add one line

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
  * Description: Initializes variables and a pointer for further operations
+ *              Prints the value of a[2]
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int n;
@@ -17,16 +15,7 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
+	printf("a[2] = %d\n", *(int *)((char *)p + 24));
+
 	return (0);
 }
